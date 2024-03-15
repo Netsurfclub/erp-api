bb9e195dd2766e6ef45b3dc35c4452632b5b0c9a
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-15 14:17:40 +0100

    Initial setup (#4)

--- src/main/kotlin/hu/netsurf/erp/Application.kt
+++ src/main/kotlin/hu/netsurf/erp/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ErpApiApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<ErpApiApplication>(*args)
+    runApplication<Application>(*args)
 }
