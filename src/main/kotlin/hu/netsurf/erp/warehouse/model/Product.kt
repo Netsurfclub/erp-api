5876ebef78ad726fdc1f16e2bf482559625c09b6
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-19 20:29:40 +0100

    Transform data classes to JPA entities.

--- src/main/kotlin/hu/netsurf/erp/warehouse/model/Product.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/model/Product.kt
@@ -1,11 +1,28 @@
 package hu.netsurf.erp.warehouse.model
 
+import hu.netsurf.erp.warehouse.constants.TableSchemaConstants.PRODUCTS
+import hu.netsurf.erp.warehouse.constants.TableSchemaConstants.SUPPLIER_ID
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = PRODUCTS)
 data class Product(
-    val id: Int,
-    val name: String,
-    val supplier: Supplier,
-    val price: Double,
-    val unit: String,
-    val photo: String?,
-    val onStock: Int,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+    val name: String = "",
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = SUPPLIER_ID)
+    val supplier: Supplier = Supplier(),
+    val price: Double = 0.0,
+    val unit: String = "",
+    val photo: String? = null,
+    val onStock: Int = 0,
 )
