79da2deb2712f4665cd827c73a989e3df9671fd3
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-19 20:43:22 +0100

    Connect to database (#16)

---
+++ src/main/kotlin/hu/netsurf/erp/warehouse/constants/TableSchemaConstants.kt
@@ -0,0 +1,7 @@
+package hu.netsurf.erp.warehouse.constants
+
+object TableSchemaConstants {
+    const val PRODUCTS = "products"
+    const val SUPPLIERS = "suppliers"
+    const val SUPPLIER_ID = "supplier_id"
+}
--- src/main/kotlin/hu/netsurf/erp/warehouse/model/Supplier.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/model/Supplier.kt
@@ -1,6 +1,17 @@
 package hu.netsurf.erp.warehouse.model
 
+import hu.netsurf.erp.warehouse.constants.TableSchemaConstants.SUPPLIERS
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = SUPPLIERS)
 data class Supplier(
-    val id: Int,
-    val name: String,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+    val name: String = "",
 )
