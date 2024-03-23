b6e2757b814394e5eed585839d105358dbd7b1d9
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-23 11:06:52 +0100

    Add ProductPhotoController.kt

---
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductPhotoController.kt
@@ -0,0 +1,11 @@
+package hu.netsurf.erp.warehouse.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ProductPhotoController {
+
+    @PostMapping
+    fun uploadProductPhoto() {}
+}
