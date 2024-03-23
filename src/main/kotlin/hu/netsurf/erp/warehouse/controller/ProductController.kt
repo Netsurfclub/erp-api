9ee71d555358f6d8c8bf4ce3d1a3f27a22e1ea1a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-23 14:39:35 +0100

    Change Controller to RestController annotation.

--- src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
@@ -3,9 +3,9 @@ package hu.netsurf.erp.warehouse.controller
 import hu.netsurf.erp.warehouse.model.Product
 import hu.netsurf.erp.warehouse.repository.ProductRepository
 import org.springframework.graphql.data.method.annotation.QueryMapping
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class ProductController(private val productRepository: ProductRepository) {
 
     @QueryMapping
