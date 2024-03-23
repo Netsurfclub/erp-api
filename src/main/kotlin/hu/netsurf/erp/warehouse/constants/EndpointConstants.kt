b21fe7a6f45a24499b143984f787d76b7c00b2e8
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-23 15:07:52 +0100

    Move strings to constant values.

---
+++ src/main/kotlin/hu/netsurf/erp/warehouse/constants/EndpointConstants.kt
@@ -0,0 +1,6 @@
+package hu.netsurf.erp.warehouse.constants
+
+object EndpointConstants {
+    const val PRODUCT_PHOTOS_ENDPOINT = "/api/product/photos"
+    const val PRODUCT_ID = "/{productId}"
+}
--- src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductPhotoController.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductPhotoController.kt
@@ -1,15 +1,17 @@
 package hu.netsurf.erp.warehouse.controller
 
+import hu.netsurf.erp.warehouse.constants.EndpointConstants.PRODUCT_ID
+import hu.netsurf.erp.warehouse.constants.EndpointConstants.PRODUCT_PHOTOS_ENDPOINT
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/api/product/photos"])
+@RequestMapping(path = [PRODUCT_PHOTOS_ENDPOINT])
 class ProductPhotoController {
 
-    @PostMapping(path = ["/{productId}"])
+    @PostMapping(path = [PRODUCT_ID])
     fun uploadProductPhoto(@PathVariable productId: Int): Int {
         return productId
     }
