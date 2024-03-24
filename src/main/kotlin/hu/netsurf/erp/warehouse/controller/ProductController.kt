43dcc62e9a5bb58a768a60a9026c52afd9714a3b
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-24 16:22:08 +0100

    Add controller endpoint for mutation.

--- src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
@@ -2,6 +2,8 @@ package hu.netsurf.erp.warehouse.controller
 
 import hu.netsurf.erp.warehouse.model.Product
 import hu.netsurf.erp.warehouse.service.ProductService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +13,10 @@ class ProductController(private val productService: ProductService) {
     @QueryMapping(name = "products")
     fun products(): List<Product> {
         return productService.getProducts()
+    }
+
+    @MutationMapping(name = "createProduct")
+    fun createProduct(@Argument input: Product): Product {
+        return productService.createProduct(input)
     }
 }
