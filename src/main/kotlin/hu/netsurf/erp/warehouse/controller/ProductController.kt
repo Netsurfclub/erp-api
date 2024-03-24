0cc581e0095d65767b7f6bf1f1925e893ccd0584
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-24 16:41:15 +0100

    Define and implement createProduct mutation (#17)

--- src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/ProductController.kt
@@ -1,7 +1,10 @@
 package hu.netsurf.erp.warehouse.controller
 
 import hu.netsurf.erp.warehouse.model.Product
+import hu.netsurf.erp.warehouse.model.ProductInput
 import hu.netsurf.erp.warehouse.service.ProductService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +14,11 @@ class ProductController(private val productService: ProductService) {
     @QueryMapping(name = "products")
     fun products(): List<Product> {
         return productService.getProducts()
+    }
+
+    @MutationMapping(name = "createProduct")
+    fun createProduct(@Argument input: ProductInput): Product {
+        val product = productService.createProduct(input)
+        return productService.getProduct(product.id)
     }
 }
