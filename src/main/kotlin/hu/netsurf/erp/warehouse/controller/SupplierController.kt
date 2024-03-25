24daec47ad0fdbe75ac58bf3efea95960127a237
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-25 06:21:22 +0100

    Implement Supplier queries and mutations (#32)

---
+++ src/main/kotlin/hu/netsurf/erp/warehouse/controller/SupplierController.kt
@@ -0,0 +1,24 @@
+package hu.netsurf.erp.warehouse.controller
+
+import hu.netsurf.erp.warehouse.model.Supplier
+import hu.netsurf.erp.warehouse.model.SupplierInput
+import hu.netsurf.erp.warehouse.service.SupplierService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SupplierController(private val supplierService: SupplierService) {
+
+    @QueryMapping(name = "suppliers")
+    fun products(): List<Supplier> {
+        return supplierService.getSuppliers()
+    }
+
+    @MutationMapping(name = "createSupplier")
+    fun createSupplier(@Argument input: SupplierInput): Supplier {
+        val supplier = supplierService.createSupplier(input)
+        return supplierService.getSupplier(supplier.id)
+    }
+}
