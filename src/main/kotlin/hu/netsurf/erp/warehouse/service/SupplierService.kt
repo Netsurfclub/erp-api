237e48cb96e0cf1387d5bfcc592aeb14ad6d8d4c
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-27 22:18:32 +0100

    Add logger.

--- src/main/kotlin/hu/netsurf/erp/warehouse/service/SupplierService.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/service/SupplierService.kt
@@ -4,10 +4,13 @@ import hu.netsurf.erp.warehouse.exception.SupplierNotFoundException
 import hu.netsurf.erp.warehouse.model.Supplier
 import hu.netsurf.erp.warehouse.model.SupplierInput
 import hu.netsurf.erp.warehouse.repository.SupplierRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class SupplierService(private val supplierRepository: SupplierRepository) {
+    val logger: Logger = LoggerFactory.getLogger(ProductService::class.java)
 
     fun getSuppliers(): List<Supplier> {
         return supplierRepository.findAll()
