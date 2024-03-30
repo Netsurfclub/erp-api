d3526b4b25df4410b882b1f138cb6f253b896281
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-30 11:01:56 +0100

    Set up logging (#30)

--- src/main/kotlin/hu/netsurf/erp/warehouse/service/ProductService.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/service/ProductService.kt
@@ -1,14 +1,24 @@
 package hu.netsurf.erp.warehouse.service
 
+import hu.netsurf.erp.common.logging.constant.warehouse.LogEventConstants.PRODUCT_INPUT_MAPPED_TO_PRODUCT
+import hu.netsurf.erp.common.logging.constant.warehouse.LogEventConstants.PRODUCT_RETRIEVED_FROM_DATABASE
+import hu.netsurf.erp.common.logging.constant.warehouse.LogEventConstants.PRODUCT_UPDATED_IN_DATABASE
+import hu.netsurf.erp.common.logging.constant.warehouse.LoggerConstants.PRODUCT
+import hu.netsurf.erp.common.logging.constant.warehouse.LoggerConstants.PRODUCT_INPUT
+import hu.netsurf.erp.common.logging.constant.warehouse.LoggerConstants.UPDATED_PRODUCT
+import hu.netsurf.erp.common.logging.extension.logInfo
 import hu.netsurf.erp.warehouse.exception.ProductNotFoundException
 import hu.netsurf.erp.warehouse.extension.toProduct
 import hu.netsurf.erp.warehouse.model.Product
 import hu.netsurf.erp.warehouse.model.ProductInput
 import hu.netsurf.erp.warehouse.repository.ProductRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class ProductService(private val productRepository: ProductRepository) {
+    val logger: Logger = LoggerFactory.getLogger(ProductService::class.java)
 
     fun getProducts(): List<Product> {
         return productRepository.findAll()
@@ -21,15 +31,40 @@ class ProductService(private val productRepository: ProductRepository) {
             throw ProductNotFoundException(id)
         }
 
+        logger.logInfo(
+            PRODUCT_RETRIEVED_FROM_DATABASE,
+            mapOf(PRODUCT to product.get()),
+        )
+
         return product.get()
     }
 
     fun createProduct(productInput: ProductInput): Product {
-        val product = productRepository.save(productInput.toProduct())
-        return getProduct(product.id)
+        val product = productInput.toProduct()
+
+        logger.logInfo(
+            PRODUCT_INPUT_MAPPED_TO_PRODUCT,
+            mapOf(
+                PRODUCT_INPUT to productInput,
+                PRODUCT to product,
+            ),
+        )
+
+        val savedProduct = productRepository.save(product)
+        return getProduct(savedProduct.id)
     }
 
     fun updateProduct(product: Product): Product {
-        return productRepository.save(product)
+        val updatedProduct = productRepository.save(product)
+
+        logger.logInfo(
+            PRODUCT_UPDATED_IN_DATABASE,
+            mapOf(
+                PRODUCT to product,
+                UPDATED_PRODUCT to updatedProduct,
+            ),
+        )
+
+        return updatedProduct
     }
 }
