77455b125c7b2a84945ae7803c5a0501f945fd38
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-01 13:56:58 +0200

    Clean up generate-database-secrets-file.sh and all references to database-secrets.yaml

--- generate-database-secrets-file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cd ./src/main/resources || exit
-
-rm -rf ./database-secrets.yml
-
-touch ./database-secrets.yml
-
-yq e -n '.jpa-connection.host = null | .jpa-connection.port = null | .jpa-connection.database = null | .jpa-connection.username = null | .jpa-connection.password = null' > ./database-secrets.yml
