639fc53b53db8b607f2b238f29f1db8ef0a9eff4
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-04 19:40:16 +0200

    Translate exception messages to hungarian.

--- src/main/kotlin/hu/netsurf/erp/warehouse/exception/InvalidFileExtensionException.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/exception/InvalidFileExtensionException.kt
@@ -1,3 +1,3 @@
 package hu.netsurf.erp.warehouse.exception
 
-class InvalidFileExtensionException(extension: String) : Exception("Invalid file extension: $extension")
+class InvalidFileExtensionException(extension: String) : Exception("Hibás fájlformátum: .$extension")
