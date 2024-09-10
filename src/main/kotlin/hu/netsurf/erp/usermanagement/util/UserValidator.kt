2ee3c80fe9c556d4a2815f3f407a9b82fd2611ba
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-10 19:34:28 +0200

    Implement empty checks.

--- src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserValidator.kt
+++ src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserValidator.kt
@@ -6,11 +6,21 @@ import org.springframework.stereotype.Component
 @Component
 class UserValidator {
     fun validate(userInput: UserInput) {
-        // 1. Empty checks.
+        if (isEmpty(userInput)) {
+            throw Exception()
+        }
         // 2. Length checks.
         // 3. 'firstName' starts with uppercase character.
         // 4. 'lastName' starts with uppercase character.
         // 5. 'email' field should contain a '@' character => Might need an email regex.
         // 6. 'password' and 'confirmPassword' fields equality check.
     }
+
+    private fun isEmpty(input: UserInput): Boolean =
+        input.username.isBlank() ||
+            input.password.isBlank() ||
+            input.confirmPassword.isBlank() ||
+            input.firstName.isBlank() ||
+            input.lastName.isBlank() ||
+            input.email.isBlank()
 }
