887fb669154ad97c96a17b33ea31f26c9f4ddde9
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-12 19:53:59 +0200

    Sanitize userInput's fields.

--- src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserInputValidator.kt
+++ src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserInputValidator.kt
@@ -4,9 +4,12 @@ import hu.netsurf.erp.usermanagement.model.UserInput
 import org.springframework.stereotype.Component
 
 @Component
-class UserInputValidator {
+class UserInputValidator(
+    private val inputSanitizer: InputSanitizer,
+) {
     fun validate(userInput: UserInput) {
-        // 0. Sanitize all input fields.
+        val sanitizedUserInput = sanitize(userInput)
+
         // 1. Empty check input fields.
         // 2. Checks length for input fields.
         // 3. 'firstName' field starts with uppercase character.
@@ -14,4 +17,14 @@ class UserInputValidator {
         // 5. 'email' field should contain a '@' character (email regex).
         // 6. 'password' and 'confirmPassword' fields equality check.
     }
+
+    private fun sanitize(userInput: UserInput): UserInput =
+        UserInput(
+            username = inputSanitizer.sanitize(userInput.username),
+            password = inputSanitizer.sanitize(userInput.password),
+            confirmPassword = inputSanitizer.sanitize(userInput.confirmPassword),
+            firstName = inputSanitizer.sanitize(userInput.firstName),
+            lastName = inputSanitizer.sanitize(userInput.lastName),
+            email = inputSanitizer.sanitize(userInput.email),
+        )
 }
