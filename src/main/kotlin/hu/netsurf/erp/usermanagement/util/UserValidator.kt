21b07bce4da4ba29412270ce416a7dcd66d12429
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-12 18:59:30 +0200

    Revert "Implement empty checks."
    This reverts commit 2ee3c80fe9c556d4a2815f3f407a9b82fd2611ba.

--- src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserValidator.kt
+++ src/main/kotlin/hu/netsurf/erp/usermanagement/util/UserValidator.kt
@@ -6,21 +6,11 @@ import org.springframework.stereotype.Component
 @Component
 class UserValidator {
     fun validate(userInput: UserInput) {
-        if (isEmpty(userInput)) {
-            throw Exception()
-        }
+        // 1. Empty checks.
         // 2. Length checks.
         // 3. 'firstName' starts with uppercase character.
         // 4. 'lastName' starts with uppercase character.
         // 5. 'email' field should contain a '@' character => Might need an email regex.
         // 6. 'password' and 'confirmPassword' fields equality check.
     }
-
-    private fun isEmpty(input: UserInput): Boolean =
-        input.username.isBlank() ||
-            input.password.isBlank() ||
-            input.confirmPassword.isBlank() ||
-            input.firstName.isBlank() ||
-            input.lastName.isBlank() ||
-            input.email.isBlank()
 }
