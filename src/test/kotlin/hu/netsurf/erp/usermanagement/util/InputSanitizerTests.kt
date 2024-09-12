e0370e4e4dffdc2070055a93a6229d906caae05b
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-12 21:15:34 +0200

    Add unit tests for InputSanitizer

---
+++ src/test/kotlin/hu/netsurf/erp/usermanagement/util/InputSanitizerTests.kt
@@ -0,0 +1,31 @@
+package hu.netsurf.erp.usermanagement.util
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.stream.Stream
+
+@SpringBootTest
+class InputSanitizerTests {
+    private val inputSanitizer: InputSanitizer = InputSanitizer()
+
+    companion object {
+        @JvmStatic
+        fun inputParams(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of("input"),
+                Arguments.of(" input"),
+                Arguments.of("input "),
+                Arguments.of(" input "),
+            )
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputParams")
+    fun `sanitize test`(input: String) {
+        val result = inputSanitizer.sanitize(input)
+        assertEquals("input", result)
+    }
+}
