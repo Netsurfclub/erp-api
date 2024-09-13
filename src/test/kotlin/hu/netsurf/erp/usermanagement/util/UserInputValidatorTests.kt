bdd367f96a3604c757717ccf5e4894cbfdc5f9dd
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-13 19:31:28 +0200

    Add unit test for UserInputValidator - email.

---
+++ src/test/kotlin/hu/netsurf/erp/usermanagement/util/UserInputValidatorTests.kt
@@ -0,0 +1,45 @@
+package hu.netsurf.erp.usermanagement.util
+
+import hu.netsurf.erp.usermanagement.model.UserInput
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+class UserInputValidatorTests {
+    private val emailAddressValidator: EmailAddressValidator = EmailAddressValidator()
+    private val userInputValidator: UserInputValidator = UserInputValidator(emailAddressValidator)
+
+    @Test
+    fun `validate test happy path`() {
+        val userInput =
+            UserInput(
+                username = "jbence",
+                password = "pAsSwOrD",
+                confirmPassword = "pAsSwOrD",
+                firstName = "Bence",
+                lastName = "Juhász",
+                email = "bjuhasz@netsurfclub.hu",
+            )
+
+        assertDoesNotThrow {
+            userInputValidator.validate(userInput)
+        }
+    }
+
+    @Test
+    fun `validate test unhappy path - invalid email address`() {
+        val userInput =
+            UserInput(
+                username = "jbence",
+                password = "pAsSwOrD",
+                confirmPassword = "pAsSwOrD",
+                firstName = "Bence",
+                lastName = "Juhász",
+                email = "email",
+            )
+
+        assertThrows<Exception> {
+            userInputValidator.validate(userInput)
+        }
+    }
+}
