f33a198ca338871cfc1b0ea538738f7d3b5de607
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-24 09:24:09 +0200

    Refactor ProductTestObject.

--- src/test/kotlin/hu/netsurf/erp/testobject/ProductTestObject.kt
+++ src/test/kotlin/hu/netsurf/erp/testobject/ProductTestObject.kt
@@ -1,40 +1,39 @@
 package hu.netsurf.erp.testobject
 
 import hu.netsurf.erp.warehouse.model.Product
+import hu.netsurf.erp.warehouse.model.Supplier
 
 class ProductTestObject {
     companion object {
-        fun product1(): Product =
-            Product(
-                id = 1,
-                name = "Product#1",
-                supplier = SupplierTestObject.supplier1(),
-                price = 10000.0,
-                unit = "pieces",
-                photo = null,
-                onStock = 10,
-            )
+        fun product1(): Product = build()
 
-        fun product1WithPhoto(): Product =
-            Product(
-                id = 1,
-                name = "Product#1",
-                supplier = SupplierTestObject.supplier1(),
-                price = 10000.0,
-                unit = "pieces",
-                photo = "7a759fbb-39d8-4b3b-af57-4266980901dc.jpeg",
-                onStock = 10,
-            )
+        fun product1WithPhoto(): Product = build(photo = "7a759fbb-39d8-4b3b-af57-4266980901dc.jpeg")
 
         fun product2(): Product =
-            Product(
+            build(
                 id = 2,
                 name = "Product#2",
-                supplier = SupplierTestObject.supplier1(),
                 price = 20000.0,
-                unit = "pieces",
-                photo = null,
                 onStock = 20,
+            )
+
+        private fun build(
+            id: Int = 1,
+            name: String = "Product#1",
+            supplier: Supplier = SupplierTestObject.supplier1(),
+            price: Double = 10000.0,
+            unit: String = "pieces",
+            photo: String? = null,
+            onStock: Int = 10,
+        ): Product =
+            Product(
+                id = id,
+                name = name,
+                supplier = supplier,
+                price = price,
+                unit = unit,
+                photo = photo,
+                onStock = onStock,
             )
     }
 }
