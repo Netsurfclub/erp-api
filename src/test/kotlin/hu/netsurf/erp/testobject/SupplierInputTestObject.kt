71cbcc1f835978e98593a14cd6e025bc95ff125d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-24 09:30:17 +0200

    Refactor SupplierInputTestObject.

--- src/test/kotlin/hu/netsurf/erp/testobject/SupplierInputTestObject.kt
+++ src/test/kotlin/hu/netsurf/erp/testobject/SupplierInputTestObject.kt
@@ -4,11 +4,17 @@ import hu.netsurf.erp.warehouse.model.SupplierInput
 
 class SupplierInputTestObject {
     companion object {
-        fun supplierInput1(): SupplierInput =
+        fun supplierInput1(): SupplierInput = build()
+
+        private fun build(
+            name: String = "Supplier#1",
+            phone: String = "555123",
+            email: String = "supplier1@test.com",
+        ): SupplierInput =
             SupplierInput(
-                name = "Supplier#1",
-                phone = "555123",
-                email = "supplier1@test.com",
+                name = name,
+                phone = phone,
+                email = email,
             )
     }
 }
