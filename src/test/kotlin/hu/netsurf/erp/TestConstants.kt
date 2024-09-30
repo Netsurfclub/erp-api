2debd75cfd74403e63ad307fd4e7c3ebb00b37c6
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-30 12:52:56 +0200

    Add more constant values.

--- src/test/kotlin/hu/netsurf/erp/TestConstants.kt
+++ src/test/kotlin/hu/netsurf/erp/TestConstants.kt
@@ -1,6 +1,7 @@
 package hu.netsurf.erp
 
 object TestConstants {
+    const val EMPTY_STRING = ""
     const val MULTIPART_FILE_SIZE = 10485760
     const val CONTENT_TYPE_IMAGE_JPEG = "image/jpeg"
     const val ORIGINAL_FILE_NAME = "file_name.jpeg"
@@ -17,4 +18,42 @@ object TestConstants {
     const val SUPPLIER_2_NAME = "Supplier#2"
     const val SUPPLIER_2_PHONE = "555456"
     const val SUPPLIER_2_EMAIL = "supplier2@test.com"
+    const val PASSWORD = "pAsSwOrD"
+    const val INVALID_PASSWORD = "pAsSwOrD1"
+    const val LONG_PASSWORD = "pAsSwOrDpAsSwOrD"
+    const val INVALID_CONFIRM_PASSWORD = "CoNfIrMpAsSwOrD"
+    const val NEW_PASSWORD = "NeWpAsSwOrD"
+    const val INVALID_NEW_PASSWORD = "NeWpAsSwOrD1"
+    const val USERNAME_1 = "jbence"
+    const val USERNAME_2 = "jgabor"
+    const val LONG_USERNAME_1 =
+        "jbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbence" +
+            "jbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbencejbence" +
+            "jbencejbencejbencejbencejben"
+    const val FIRST_NAME_1 = "Bence"
+    const val INVALID_FIRST_NAME_1 = "bence"
+    const val INVALID_FIRST_NAME_STARTS_WITH_NUMBER = "1firstName"
+    const val FIRST_NAME_2 = "Gábor"
+    const val LONG_FIRST_NAME_1 =
+        "BenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBence" +
+            "BenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBenceBence" +
+            "BenceBenceBenceBenceBenceBenceB"
+    const val LAST_NAME_1 = "Juhász"
+    const val INVALID_LAST_NAME_1 = "juhász"
+    const val INVALID_LAST_NAME_STARTS_WITH_NUMBER = "1lastName"
+    const val LONG_LAST_NAME_1 =
+        "JuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhász" +
+            "JuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhászJuhász" +
+            "JuhászJuhászJuhászJuhászJuhászJuhá"
+    const val EMAIL_1 = "bjuhasz@netsurfclub.hu"
+    const val EMAIL_2 = "gjuhasz@netsurfclub.hu"
+    const val INVALID_EMAIL_1 = "bjuhasznetsurfclub.hu"
+    const val LONG_EMAIL_1 = "bjuhasz@netsurfclub.hubjuhasz@netsurfclub.hubjuhasz@netsurfclub.hubjuhasz@ne"
+    const val INPUT = "input"
+    const val USERNAME_INPUT = "username"
+    const val PASSWORD_INPUT = "password"
+    const val CONFIRM_PASSWORD_INPUT = "confirmPassword"
+    const val FIRST_NAME_INPUT = "firstName"
+    const val LAST_NAME_INPUT = "lastName"
+    const val EMAIL_INPUT = "email"
 }
