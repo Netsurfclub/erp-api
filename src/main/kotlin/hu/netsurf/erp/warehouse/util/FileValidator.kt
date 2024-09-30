a7b6e5cd3104861b03cfe3c2d752afc3356c75d5
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-09-30 18:14:13 +0200

    Replace MultipartFile related validation exceptions to 'MultipartFileValidationResult'.

--- src/main/kotlin/hu/netsurf/erp/warehouse/util/FileValidator.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/util/FileValidator.kt
@@ -1,9 +1,8 @@
 ﻿package hu.netsurf.erp.warehouse.util
 
 import hu.netsurf.erp.warehouse.config.FileExtensionsConfig
-import hu.netsurf.erp.warehouse.exception.EmptyFileException
-import hu.netsurf.erp.warehouse.exception.InvalidFileExtensionException
 import hu.netsurf.erp.warehouse.extension.getExtension
+import hu.netsurf.erp.warehouse.model.MultipartFileValidationResult
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 
@@ -11,15 +10,17 @@ import org.springframework.web.multipart.MultipartFile
 class FileValidator(
     val fileExtensionsConfig: FileExtensionsConfig,
 ) {
-    fun validate(file: MultipartFile) {
+    fun validate(file: MultipartFile): MultipartFileValidationResult {
         if (file.isEmpty) {
-            throw EmptyFileException(fileName = file.originalFilename)
+            return MultipartFileValidationResult.emptyFile(file.originalFilename)
         }
 
         val fileExtension = file.getExtension()
 
         if (fileExtensionsConfig.allowedExtensions.all { it != fileExtension }) {
-            throw InvalidFileExtensionException(extension = fileExtension)
+            return MultipartFileValidationResult.invalidFileExtension(fileExtension)
         }
+
+        return MultipartFileValidationResult.success()
     }
 }
