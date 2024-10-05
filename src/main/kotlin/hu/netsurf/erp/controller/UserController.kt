d5aac363fd4edb5bcf016fd4bf4043f271c4ca6d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-05 10:42:26 +0200

    Fix UserService not to receive input types as params.

--- src/main/kotlin/hu/netsurf/erp/controller/UserController.kt
+++ src/main/kotlin/hu/netsurf/erp/controller/UserController.kt
@@ -6,11 +6,19 @@ import hu.netsurf.erp.constant.LogEventConstants.UPDATE_USER_PASSWORD_GRAPHQL_MU
 import hu.netsurf.erp.constant.LogEventConstants.UPDATE_USER_PASSWORD_GRAPHQL_MUTATION_SUCCESS_RESPONSE
 import hu.netsurf.erp.constant.LogEventConstants.USERS_GRAPHQL_QUERY_RECEIVED
 import hu.netsurf.erp.constant.LogEventConstants.USERS_GRAPHQL_QUERY_SUCCESS_RESPONSE
+import hu.netsurf.erp.constant.LogEventConstants.USER_INPUT_MAPPED_TO_USER
+import hu.netsurf.erp.constant.LoggerConstants.USER
+import hu.netsurf.erp.constant.LoggerConstants.USER_INPUT
 import hu.netsurf.erp.extension.logInfo
+import hu.netsurf.erp.extension.toUser
 import hu.netsurf.erp.model.UpdateUserPasswordInput
 import hu.netsurf.erp.model.User
 import hu.netsurf.erp.model.UserInput
 import hu.netsurf.erp.service.UserService
+import hu.netsurf.erp.util.UpdateUserPasswordInputSanitizer
+import hu.netsurf.erp.util.UpdateUserPasswordInputValidator
+import hu.netsurf.erp.util.UserInputSanitizer
+import hu.netsurf.erp.util.UserInputValidator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.graphql.data.method.annotation.Argument
@@ -21,6 +29,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class UserController(
     private val userService: UserService,
+    private val userInputSanitizer: UserInputSanitizer,
+    private val userInputValidator: UserInputValidator,
+    private val updateUserPasswordInputSanitizer: UpdateUserPasswordInputSanitizer,
+    private val updateUserPasswordInputValidator: UpdateUserPasswordInputValidator,
 ) {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
@@ -41,11 +53,24 @@ class UserController(
     ): User {
         logger.logInfo(CREATE_USER_GRAPHQL_MUTATION_RECEIVED)
 
-        val user = userService.createUser(input)
+        val sanitizedUserInput = userInputSanitizer.sanitize(input)
+        userInputValidator.validate(sanitizedUserInput)
+
+        val user = sanitizedUserInput.toUser()
+
+        logger.logInfo(
+            USER_INPUT_MAPPED_TO_USER,
+            mapOf(
+                USER_INPUT to sanitizedUserInput,
+                USER to user,
+            ),
+        )
+
+        val createdUser = userService.createUser(user)
 
         logger.logInfo(CREATE_USER_GRAPHQL_MUTATION_SUCCESS_RESPONSE)
 
-        return user
+        return createdUser
     }
 
     @MutationMapping(name = "updateUserPassword")
@@ -54,10 +79,18 @@ class UserController(
     ): User {
         logger.logInfo(UPDATE_USER_PASSWORD_GRAPHQL_MUTATION_RECEIVED)
 
-        val user = userService.updateUserPassword(input)
+        val sanitizedUserInput = updateUserPasswordInputSanitizer.sanitize(input)
+
+        val user = userService.getUser(input.userId)
+
+        updateUserPasswordInputValidator.validate(sanitizedUserInput, user.password)
+
+        user.password = input.newPassword
+
+        val updatedUser = userService.updateUser(user)
 
         logger.logInfo(UPDATE_USER_PASSWORD_GRAPHQL_MUTATION_SUCCESS_RESPONSE)
 
-        return user
+        return updatedUser
     }
 }
