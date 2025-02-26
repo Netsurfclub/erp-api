24b8007e7e9735a1b76285fb659f138ea222799b
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-02-26 17:36:31 +0100

    Implement SupplierInputValidator.

---
+++ src/main/kotlin/hu/netsurf/erp/util/SupplierInputValidator.kt
@@ -0,0 +1,31 @@
+﻿package hu.netsurf.erp.util
+
+import hu.netsurf.erp.exception.EmptyFieldException
+import hu.netsurf.erp.exception.InvalidEmailAddressFormatException
+import hu.netsurf.erp.exception.InvalidLengthException
+import hu.netsurf.erp.input.SupplierInput
+import org.springframework.stereotype.Component
+
+@Component
+class SupplierInputValidator {
+    fun validate(supplierInput: SupplierInput) {
+        if (supplierInput.nameIsEmpty()) {
+            throw EmptyFieldException()
+        }
+
+        if (
+            supplierInput.nameIsShort() ||
+            supplierInput.nameIsLong() ||
+            supplierInput.emailIsShort() ||
+            supplierInput.emailIsLong() ||
+            supplierInput.phoneIsShort() ||
+            supplierInput.phoneIsLong()
+        ) {
+            throw InvalidLengthException()
+        }
+
+        if (!supplierInput.emailAddressIsValid()) {
+            throw InvalidEmailAddressFormatException()
+        }
+    }
+}
--- src/test/kotlin/hu/netsurf/erp/testobject/SupplierInputTestObject.kt
+++ src/test/kotlin/hu/netsurf/erp/testobject/SupplierInputTestObject.kt
@@ -1,13 +1,34 @@
 package hu.netsurf.erp.testobject
 
+import hu.netsurf.erp.TestConstants.EMPTY_STRING
 import hu.netsurf.erp.TestConstants.SUPPLIER_1_EMAIL
 import hu.netsurf.erp.TestConstants.SUPPLIER_1_NAME
 import hu.netsurf.erp.TestConstants.SUPPLIER_1_PHONE
+import hu.netsurf.erp.TestConstants.SUPPLIER_INVALID_EMAIL
+import hu.netsurf.erp.TestConstants.SUPPLIER_LONG_EMAIL
+import hu.netsurf.erp.TestConstants.SUPPLIER_LONG_NAME
+import hu.netsurf.erp.TestConstants.SUPPLIER_LONG_PHONE
 import hu.netsurf.erp.input.SupplierInput
 
 class SupplierInputTestObject {
     companion object {
         fun supplierInput1(): SupplierInput = build()
+
+        fun supplierInput1WithEmptyName(): SupplierInput = build(name = EMPTY_STRING)
+
+        fun supplierInput1WithShortName(): SupplierInput = build(name = "a")
+
+        fun supplierInput1WithLongName(): SupplierInput = build(name = SUPPLIER_LONG_NAME)
+
+        fun supplierInput1WithShortPhone(): SupplierInput = build(phone = "1")
+
+        fun supplierInput1WithLongPhone(): SupplierInput = build(phone = SUPPLIER_LONG_PHONE)
+
+        fun supplierInput1WithShortEmail(): SupplierInput = build(email = "a")
+
+        fun supplierInput1WithLongEmail(): SupplierInput = build(email = SUPPLIER_LONG_EMAIL)
+
+        fun supplierInput1WithInvalidEmail(): SupplierInput = build(email = SUPPLIER_INVALID_EMAIL)
 
         private fun build(
             name: String = SUPPLIER_1_NAME,
