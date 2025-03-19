836e53caa0a69d46a5dad02863e031c9293b8216
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-03-19 17:28:03 +0100

    Fix ProductPhotoServiceTests.

--- src/test/kotlin/hu/netsurf/erp/TestConstants.kt
+++ src/test/kotlin/hu/netsurf/erp/TestConstants.kt
@@ -2,13 +2,14 @@ package hu.netsurf.erp
 
 object TestConstants {
     const val EMPTY_STRING = ""
-    const val MULTIPART_FILE_SIZE = 10485760
+    const val FILE_SIZE = 10485760
     const val CONTENT_TYPE_IMAGE_JPEG = "image/jpeg"
     const val ORIGINAL_FILE_NAME = "file_name.jpeg"
     const val ORIGINAL_FILE_EXTENSION = "jpeg"
     const val INVALID_ORIGINAL_FILE_NAME = "file_name.txt"
     const val INVALID_ORIGINAL_FILE_EXTENSION = "txt"
     const val PHOTO_FILE_NAME = "7a759fbb-39d8-4b3b-af57-4266980901dc.jpeg"
+    const val PHOTO_FILE_AS_STRING = "{originalFileName=${ORIGINAL_FILE_NAME} size=${FILE_SIZE} contentType=${CONTENT_TYPE_IMAGE_JPEG}}"
     const val UPLOADS_DIRECTORY_WITH_PHOTOS_SUBDIRECTORY_AND_CUSTOM_SUBDIRECTORY = "uploads/photos/products/"
     val ALLOWED_EXTENSIONS = listOf("jpg", "jpeg", "png", "bmp")
     const val PRODUCT_1_NAME = "Product#1"
