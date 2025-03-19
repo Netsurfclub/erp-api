6f80923aaf50094810795e13b242c4982fe6b2b0
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-03-19 19:04:38 +0100

    Refactor test constants.

--- src/test/kotlin/hu/netsurf/erp/TestConstants.kt
+++ src/test/kotlin/hu/netsurf/erp/TestConstants.kt
@@ -3,16 +3,20 @@ package hu.netsurf.erp
 object TestConstants {
     const val EMPTY_STRING = ""
     const val FILE_SIZE = 10485760
+    private const val JPG = "jpg"
+    private const val JPEG = "jpeg"
+    private const val PNG = "png"
+    private const val BMP = "bmp"
     const val CONTENT_TYPE_IMAGE_JPEG = "image/jpeg"
     const val FILE_NAME = "file_name.jpeg"
     const val ORIGINAL_FILE_NAME = FILE_NAME
-    const val ORIGINAL_FILE_EXTENSION = "jpeg"
+    const val ORIGINAL_FILE_EXTENSION = JPEG
     const val INVALID_ORIGINAL_FILE_NAME = "file_name.txt"
     const val INVALID_ORIGINAL_FILE_EXTENSION = "txt"
     const val PHOTO_FILE_NAME = "7a759fbb-39d8-4b3b-af57-4266980901dc.jpeg"
     const val PHOTO_FILE_AS_STRING = "{originalFileName=${ORIGINAL_FILE_NAME} size=${FILE_SIZE} contentType=${CONTENT_TYPE_IMAGE_JPEG}}"
     const val UPLOADS_DIRECTORY_WITH_PHOTOS_SUBDIRECTORY_AND_CUSTOM_SUBDIRECTORY = "uploads/photos/products/"
-    val ALLOWED_EXTENSIONS = listOf("jpg", "jpeg", "png", "bmp")
+    val ALLOWED_EXTENSIONS = listOf(JPG, JPEG, PNG, BMP)
     const val PRODUCT_1_NAME = "Product#1"
     const val PRODUCT_1_UNIT = "pieces"
     const val PRODUCT_2_NAME = "Product#2"
