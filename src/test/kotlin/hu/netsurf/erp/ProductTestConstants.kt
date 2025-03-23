186c0c1be4869203360ead8d6cb85d73cd28adbe
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-03-23 10:22:19 +0100

    Implement ProductInputValidator.

--- src/test/kotlin/hu/netsurf/erp/ProductTestConstants.kt
+++ src/test/kotlin/hu/netsurf/erp/ProductTestConstants.kt
@@ -4,4 +4,6 @@ object ProductTestConstants {
     const val PRODUCT_1_NAME = "Product#1"
     const val PRODUCT_1_UNIT = "pieces"
     const val PRODUCT_2_NAME = "Product#2"
+    const val PRODUCT_LONG_NAME = "Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Product#1Prod"
+    const val PRODUCT_LONG_UNIT = "kilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilogramskilo"
 }
