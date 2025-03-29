fbe2bdd60973824871c58d19f68cc3e081f6af32
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-03-29 09:08:28 +0100

    Using import aliases.

--- src/main/kotlin/hu/netsurf/erp/common/extension/LoggerExtensions.kt
+++ src/main/kotlin/hu/netsurf/erp/common/extension/LoggerExtensions.kt
@@ -2,19 +2,21 @@ package hu.netsurf.erp.common.extension
 
 import hu.netsurf.erp.common.constant.CommonConstants.EMPTY_STRING
 import org.slf4j.Logger
+import hu.netsurf.erp.usermanagement.constant.LogEventConstants as UserManagementLogEventConstants
+import hu.netsurf.erp.warehouse.constant.LogEventConstants as WarehouseLogEventConstants
 
 fun Logger.logInfo(
-    logEventConstants: hu.netsurf.erp.warehouse.constant.LogEventConstants,
+    logEventConstants: WarehouseLogEventConstants,
     additionalProperties: Map<String, Any> = emptyMap(),
 ) = this.info("${logEventConstants.eventName} ${logEventConstants.eventMessage} ${format(additionalProperties)}")
 
 fun Logger.logError(
-    logEventConstants: hu.netsurf.erp.warehouse.constant.LogEventConstants,
+    logEventConstants: WarehouseLogEventConstants,
     exception: Exception,
 ) = this.error("${logEventConstants.eventName} ${logEventConstants.eventMessage}: ${exception.message}")
 
 fun Logger.logInfo(
-    logEventConstants: hu.netsurf.erp.usermanagement.constant.LogEventConstants,
+    logEventConstants: UserManagementLogEventConstants,
     additionalProperties: Map<String, Any> = emptyMap(),
 ) = this.info("${logEventConstants.eventName} ${logEventConstants.eventMessage} ${format(additionalProperties)}")
 
