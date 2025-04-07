cc7ebb4270544b9226edeed510a8d05d6bf1bf79
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-07 16:41:45 +0200

    Add SecurityConfig to the EnableConfigurationProperties annotation params.

--- src/main/kotlin/hu/netsurf/erp/Application.kt
+++ src/main/kotlin/hu/netsurf/erp/Application.kt
@@ -1,12 +1,18 @@
 package hu.netsurf.erp
 
+import hu.netsurf.erp.usermanagement.config.SecurityConfig
 import hu.netsurf.erp.warehouse.config.FileExtensionsConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = [FileExtensionsConfig::class])
+@EnableConfigurationProperties(
+    value = [
+        SecurityConfig::class,
+        FileExtensionsConfig::class,
+    ],
+)
 class Application
 
 fun main(args: Array<String>) {
