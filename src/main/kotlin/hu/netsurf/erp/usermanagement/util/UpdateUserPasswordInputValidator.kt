c222b861ced348a003e6191741fab32ae5fe4524
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-08 17:58:32 +0200

    Remove validation for checking new password and the current password in database is matching or not.

--- src/main/kotlin/hu/netsurf/erp/usermanagement/util/UpdateUserPasswordInputValidator.kt
+++ src/main/kotlin/hu/netsurf/erp/usermanagement/util/UpdateUserPasswordInputValidator.kt
@@ -3,33 +3,28 @@
 import hu.netsurf.erp.common.exception.EmptyFieldException
 import hu.netsurf.erp.usermanagement.exception.CurrentPasswordAndPasswordInDatabaseNotMatchesException
 import hu.netsurf.erp.usermanagement.exception.NewPasswordAndConfirmNewPasswordNotMatchesException
-import hu.netsurf.erp.usermanagement.exception.NewPasswordAndPasswordInDatabaseMatchesException
 import hu.netsurf.erp.usermanagement.input.UpdateUserPasswordInput
 import org.springframework.stereotype.Component
 
 @Component
 class UpdateUserPasswordInputValidator {
     fun validate(
-        updateUserPasswordInput: UpdateUserPasswordInput,
+        input: UpdateUserPasswordInput,
         passwordInDatabase: String,
     ) {
         if (
-            updateUserPasswordInput.currentPasswordIsEmpty() ||
-            updateUserPasswordInput.newPasswordIsEmpty() ||
-            updateUserPasswordInput.confirmNewPasswordIsEmpty()
+            input.currentPasswordIsEmpty() ||
+            input.newPasswordIsEmpty() ||
+            input.confirmNewPasswordIsEmpty()
         ) {
             throw EmptyFieldException()
         }
 
-        if (!updateUserPasswordInput.currentPasswordAndPasswordInDatabaseMatches(passwordInDatabase)) {
+        if (!input.currentPasswordAndPasswordInDatabaseMatches(passwordInDatabase)) {
             throw CurrentPasswordAndPasswordInDatabaseNotMatchesException()
         }
 
-        if (updateUserPasswordInput.newPasswordAndCurrentPasswordInDatabaseMatches(passwordInDatabase)) {
-            throw NewPasswordAndPasswordInDatabaseMatchesException()
-        }
-
-        if (!updateUserPasswordInput.newPasswordAndConfirmNewPasswordMatches()) {
+        if (!input.newPasswordAndConfirmNewPasswordMatches()) {
             throw NewPasswordAndConfirmNewPasswordNotMatchesException()
         }
     }
--- src/test/kotlin/hu/netsurf/erp/usermanagement/testobject/DeleteUserInputTestObject.kt
+++ src/test/kotlin/hu/netsurf/erp/usermanagement/testobject/DeleteUserInputTestObject.kt
@@ -4,7 +4,7 @@ import hu.netsurf.erp.usermanagement.input.DeleteUserInput
 
 class DeleteUserInputTestObject {
     companion object {
-        fun deleteUserInput1(): DeleteUserInput = build()
+        fun input1(): DeleteUserInput = build()
 
         private fun build(userId: Int = 1): DeleteUserInput = DeleteUserInput(userId = userId)
     }
