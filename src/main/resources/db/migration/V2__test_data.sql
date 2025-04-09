0f56db544ebb9f266a97916127a639aaed51a060
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-09 16:40:56 +0200

    Update test data.

--- src/main/resources/db/migration/V2__test_data.sql
+++ src/main/resources/db/migration/V2__test_data.sql
@@ -9,6 +9,7 @@ VALUES ('6-os csavar', 1, 120, 'darab', NULL, 1000),
        ('Vörösréz', 1, 3000, 'kilogramm', NULL, 3000);
 
 INSERT INTO users (username, password, first_name, last_name, email, is_deleted)
-VALUES ('bjuhasz', '12345', 'Ferenc', 'Dunai', 'juhasz.bence@netsurfclub.hu', 0),
-       ('gjuhasz', '12345', 'Gábor', 'Juhász', 'juhasz.gabor@netsurfclub.hu', 0),
-       ('lpapp', '12345', 'László', 'Papp', 'papp.laszlo@netsurfclub.hu', 0);
+VALUES ('bjuhasz', '$2a$10$HF7OVtgqLl7SQpb3Cdue.u.JNEtPmnzcHcXq5fqIp3VihXENxofNO', 'Bence', 'Juhász', 'juhasz.bence@netsurfclub.hu', 0),
+       ('gjuhasz', '$2a$10$0Qhoqu0M5gl6zpWAc90QH.fz7JG8rjtE..VnVXSEQV15G3USUZAAu', 'Gábor', 'Juhász', 'juhasz.gabor@netsurfclub.hu', 0),
+       ('lpapp', '$2a$10$SNQfcKuFrPgXWGjeWotgLOVLs9VuNBdQllxwVYHZeZOdLhsruiB..', 'László', 'Papp', 'papp.laszlo@netsurfclub.hu', 0),
+       ('fdunai', '$2a$10$mpQ8nod5twYJ7Mbx9zeKoecGbe14PxG6rgWEcRyL0WgXRzxnE5hkG', 'Ferenc', 'Dunai', 'ferenc.dunai@netsurfclub.hu', 0);
