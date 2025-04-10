a9e16980c1de640d5de1213ba6e271e000396d23
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-10 17:34:33 +0200

    Reduce duplicated constants.

--- src/main/kotlin/hu/netsurf/erp/warehouse/constant/ProductValidationConstants.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/constant/ProductValidationConstants.kt
@@ -1,8 +1,6 @@
 package hu.netsurf.erp.warehouse.constant
 
 object ProductValidationConstants {
-    const val NAME_MIN_LENGTH = 1
-    const val NAME_MAX_LENGTH = 255
     const val UNIT_MIN_LENGTH = 1
     const val UNIT_MAX_LENGTH = 255
 }
