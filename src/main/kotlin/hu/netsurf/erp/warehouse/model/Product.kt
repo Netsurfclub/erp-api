2207f5a914e1ad5fd8d1b690ae1c4af0be885e51
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-10 17:18:44 +0200

    Removing table schema constants.

--- src/main/kotlin/hu/netsurf/erp/warehouse/model/Product.kt
+++ src/main/kotlin/hu/netsurf/erp/warehouse/model/Product.kt
@@ -1,7 +1,5 @@
 package hu.netsurf.erp.warehouse.model
 
-import hu.netsurf.erp.warehouse.constant.TableSchemaConstants.COLUMN_NAME_SUPPLIER_ID
-import hu.netsurf.erp.warehouse.constant.TableSchemaConstants.TABLE_NAME_PRODUCTS
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
@@ -12,14 +10,14 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = TABLE_NAME_PRODUCTS)
+@Table(name = "products")
 data class Product(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int = 0,
     var name: String = "",
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = COLUMN_NAME_SUPPLIER_ID)
+    @JoinColumn(name = "supplier_id")
     var supplier: Supplier = Supplier(),
     var price: Double = 0.0,
     var unit: String = "",
