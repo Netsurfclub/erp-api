dd3d8c3c05dd2faadc3787294ad65a47cb1de813
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-04-20 18:22:27 +0200

    Fix ktlint errors.

--- src/test/kotlin/hu/netsurf/erp/warehouse/testobject/UpdateSupplierInputTestObject.kt
+++ src/test/kotlin/hu/netsurf/erp/warehouse/testobject/UpdateSupplierInputTestObject.kt
@@ -1,12 +1,12 @@
 package hu.netsurf.erp.warehouse.testobject
 
 import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_EMAIL
+import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_INVALID_EMAIL
+import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_LONG_EMAIL
+import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_LONG_NAME
+import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_LONG_PHONE
 import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_NAME
 import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_1_PHONE
-import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_INVALID_EMAIL
-import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_LONG_EMAIL
-import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_LONG_NAME
-import hu.netsurf.erp.warehouse.constant.SupplierTestConstants.SUPPLIER_LONG_PHONE
 import hu.netsurf.erp.warehouse.input.UpdateSupplierInput
 
 class UpdateSupplierInputTestObject {
@@ -15,21 +15,21 @@ class UpdateSupplierInputTestObject {
 
         fun input1WithShortName(): UpdateSupplierInput = build(name = "a")
 
-        fun input1WithLongName(): UpdateSupplierInput = build(name = SUPPLIER_LONG_NAME)
+        fun input1WithLongName(): UpdateSupplierInput = build(name = SUPPLIER_1_LONG_NAME)
 
         fun input1WithNullName(): UpdateSupplierInput = build(name = null)
 
         fun input1WithShortPhone(): UpdateSupplierInput = build(phone = "1")
 
-        fun input1WithLongPhone(): UpdateSupplierInput = build(phone = SUPPLIER_LONG_PHONE)
+        fun input1WithLongPhone(): UpdateSupplierInput = build(phone = SUPPLIER_1_LONG_PHONE)
 
         fun input1WithNullPhone(): UpdateSupplierInput = build(phone = null)
 
         fun input1WithShortEmail(): UpdateSupplierInput = build(email = "a")
 
-        fun input1WithLongEmail(): UpdateSupplierInput = build(email = SUPPLIER_LONG_EMAIL)
+        fun input1WithLongEmail(): UpdateSupplierInput = build(email = SUPPLIER_1_LONG_EMAIL)
 
-        fun input1WithInvalidEmail(): UpdateSupplierInput = build(email = SUPPLIER_INVALID_EMAIL)
+        fun input1WithInvalidEmail(): UpdateSupplierInput = build(email = SUPPLIER_1_INVALID_EMAIL)
 
         fun input1WithNullEmail(): UpdateSupplierInput = build(email = null)
 
